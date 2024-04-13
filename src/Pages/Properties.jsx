a901d0ed62d619c99beb093f936f0227fc0e5edb
Riyad Hosen <riyadhosen@Riyads-Air.netis.cc>
2024-04-12 19:58:55 -0700

    added estate section

--- src/Pages/Properties.jsx
+++ src/Pages/Properties.jsx
@@ -29,7 +29,7 @@ const Properties = () => {
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8 ">
                 {
-                    properties.map(property => <div key={property.id}><Property/></div>)
+                    properties.map(property => <div key={property.id}><Property property={property}/></div>)
                 }
             </div>
         </div>
--- src/components/Property.jsx
+++ src/components/Property.jsx
@@ -1,10 +1,34 @@
+import { IoLocationOutline } from "react-icons/io5";
+import { FaChartArea } from "react-icons/fa";
 
-const Property = () => {
-    return (
-        <div className="border-2">
-            <h1>Property</h1>
-        </div>
-    );
+const Property = ({ property }) => {
+  console.log(property);
+  const { image, estate_title, location, area, segment_name, price, status } =
+    property;
+  return (
+    <div className="border-2 rounded-xl shadow-lg bg-white relative">
+      <div
+        className="h-64 bg-cover"
+        style={{
+          backgroundImage: `url(${image})`,
+        }}
+      ></div>
+      <div className="absolute top-[190px] text-white text-3xl font-bold left-4">
+        <p>{price}</p>
+      </div>
+      <div className="absolute top-[230px] left-4">
+        <h1 className="bg-white px-4 p-1 rounded-t-lg">{segment_name}</h1>
+      </div>
+      <div className="p-4 space-y-4">
+        <h1 className="text-2xl font-bold">{estate_title}</h1>
+        <h2 className="text-xl flex items-center gap-1"><IoLocationOutline color="#3fb89a" size={30}/>{location}</h2>
+        <p className="text-lg flex items-center gap-1"><FaChartArea color="#3fb89a" size={30}/> {area}</p>
+      </div>
+      <div className="text-center my-4">
+        <button className="md:px-6 px-2 py-2 text-white bg-[#3fb89a] font-bold hover:bg-[#398572] hover:scale-110 transition-transform">View Property</button>
+      </div>
+    </div>
+  );
 };
 
 export default Property;
